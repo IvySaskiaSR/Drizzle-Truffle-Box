2f9f818f40b541ff327fb37b6de4c9ba7c6d3999
IvySaskiaSR <ivy.sejas@ucb.edu.bo>
2022-02-07 00:23:27 -0400

    Added configuration for deploying to the live network

--- truffle-config.js
+++ truffle-config.js
@@ -9,6 +9,15 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*"
+    },
+    "live": {
+      network_id: 1,
+      host: "127.0.0.1",
+      port: 8546   // Different than the default below
     }
+  },
+  rpc: {
+    host: "127.0.0.1",
+    port: 8545
   }
 };
