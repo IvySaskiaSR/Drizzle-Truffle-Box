8866828d02b8dc6a41781bc30b159de046f1b8cf
IvySaskiaSR <ivy.sejas@ucb.edu.bo>
2022-02-07 00:51:28 -0400

    Updates

---
+++ precache-manifest.0147f974d2d032f93442a60d25e8c256.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02ae848fe87a81c69b7d308c79d39679",
+    "url": "/Drizzle-Truffle-Box/index.html"
+  },
+  {
+    "revision": "affea6f816be75ffafb4",
+    "url": "/Drizzle-Truffle-Box/static/css/main.54a0cdb6.chunk.css"
+  },
+  {
+    "revision": "a756046817ceb69982d8",
+    "url": "/Drizzle-Truffle-Box/static/js/2.073f2e55.chunk.js"
+  },
+  {
+    "revision": "affea6f816be75ffafb4",
+    "url": "/Drizzle-Truffle-Box/static/js/main.2b16254a.chunk.js"
+  },
+  {
+    "revision": "d89b1172b822aed32e1c",
+    "url": "/Drizzle-Truffle-Box/static/js/runtime-main.bf7c0921.js"
+  }
+]);
